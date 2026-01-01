25c86363200226c88847e4ece007ef2947a1a355
Eldar-005 <eldarhamidov2009@Gmail.com>
2026-01-01 20:49:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-/* ===== LANGUAGE SWITCH LOGIC ===== */
-
 function setLang(lang) {
     document.querySelectorAll("[data-en]").forEach(el => {
         el.innerText = el.getAttribute(`data-${lang}`);
     });
 }
-
-
